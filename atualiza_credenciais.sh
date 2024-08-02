6762826e1a69b8fdd05fdee5a00836a8a3c03fb0
Matheus Brandão <matheushfbrandao@gmail.com>
2024-08-02 14:44:11 -0300

    Script para pegar credenciais temporarias AWS

--- atualiza_credenciais.sh
+++ atualiza_credenciais.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-keys=("aws_access_key_id" "aws_secret_access_key" "aws_session_token")
+keys=(aws_access_key_id aws_secret_access_key aws_session_token)
 commando=$(aws sts get-session-token | grep -E "Access|Token" | cut -f4 -d \")
 j=0
 echo [default]
@@ -7,7 +7,7 @@ while [ $j -lt 3 ]
 do
     for i in $commando
     do
-        ${keys[$j]}=$i 
+        echo ${keys[$j]}=$i 
         j=$[$j+1]
     done
 done
