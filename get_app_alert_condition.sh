88d9c9d022a74a6e3cb614511a9849fa6e3e648b
Matheushfb <matheushfbrandao@gmail.com>
2025-03-13 17:37:59 -0300

    Commit inicial

--- get_app_alert_condition.sh
+++ get_app_alert_condition.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-
-policyID=("5706072" "5751316")
+API_KEY="XYZ"
+policyID=("0000000" "0000000")
 
 for i in ${policyID[@]}
 
 do
   saida=$(curl --silent --fail -X 'GET' \
     "https://api.newrelic.com/v2/alerts_nrql_conditions.json?policy_id=$i&page=[1-4]" \
-    -H  "X-Api-Key: NRAK-9MSB8KFMACRL03880GO15DMVBVC" \
+    -H  "X-Api-Key: $API_KEY" \
     -H 'accept: application/json')
 
   echo "$saida" | jq -r '.nrql_conditions[].name' | awk -F '|' '{print $1}' | sort > $i.txt
