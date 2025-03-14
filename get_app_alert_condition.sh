aef6fdb783aa9756ce76e95eaca13419a03b56dc
Matheushfb <matheushfbrandao@gmail.com>
2025-03-14 14:43:35 -0300

    Commit 001

--- get_app_alert_condition.sh
+++ get_app_alert_condition.sh
@@ -3,8 +3,20 @@
 API_KEY="XYZ"
 policyID=("00000000" "0000000")
 
-for i in ${policyID[@]}
 
+declare -A policyNames
+
+policies_json=$(curl --silent --fail -X 'GET' \
+  "https://api.newrelic.com/v2/alerts_policies.json" \
+  -H "X-Api-Key: $API_KEY" \
+  -H 'accept: application/json')
+
+for id in "${policyID[@]}"; do
+  name=$(echo "$policies_json" | jq -r ".policies[] | select(.id == $id) | .name")
+  policyNames["$id"]="$name"
+done
+
+for i in ${policyID[@]}
 do
   saida=$(curl --silent --fail -X 'GET' \
     "https://api.newrelic.com/v2/alerts_nrql_conditions.json?policy_id=$i&page=[1-4]" \
