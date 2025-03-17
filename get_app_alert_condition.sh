425ec081fe5613423d8b8d0f2ee8e6ff75616567
Matheushfb <matheushfbrandao@gmail.com>
2025-03-17 11:18:46 -0300

    Commit 002

--- get_app_alert_condition.sh
+++ get_app_alert_condition.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-API_KEY="XYZ"
-policyID=("00000000" "0000000")
+API_KEY="NRAK-9MSB8KFMACRL03880GO15DMVBVC"
+policyID=("5706072" "5751316") 
 
 
 declare -A policyNames
@@ -16,26 +16,33 @@ for id in "${policyID[@]}"; do
   policyNames["$id"]="$name"
 done
 
-for i in ${policyID[@]}
-do
-  saida=$(curl --silent --fail -X 'GET' \
-    "https://api.newrelic.com/v2/alerts_nrql_conditions.json?policy_id=$i&page=[1-4]" \
-    -H  "X-Api-Key: $API_KEY" \
-    -H 'accept: application/json')
+for id in "${policyID[@]}"; do
+  policy_name="${policyNames[$id]}"
+  file_name="${policy_name// /_}.txt"  # Substitui espaços por "_"
 
-  echo "$saida" | jq -r '.nrql_conditions[].name' | awk -F '|' '{print $1}' | sort > $i.txt
+  echo "🔎 Processando Policy: $policy_name (ID: $id)"
+  > "$file_name"  # Limpa o arquivo antes de escrever
+
+  for page in {1..4}; do
+    saida=$(curl --silent --fail -X 'GET' \
+      "https://api.newrelic.com/v2/alerts_nrql_conditions.json?policy_id=$id&page=$page" \
+      -H "X-Api-Key: $API_KEY" \
+      -H 'accept: application/json')
+
+    if [[ -n "$saida" ]]; then
+      echo "$saida" | jq -r '.nrql_conditions[].name' | awk -F '|' '{print $1}' >> "$file_name"
+    fi
+  done
+
+  sort -o "$file_name" "$file_name"
 done
 
-validate=$(diff ${policyID[0]}.txt ${policyID[1]}.txt)
+file1="${policyNames[${policyID[0]}]// /_}.txt"
+file2="${policyNames[${policyID[1]}]// /_}.txt"
 
-if [[ $validate -eq 0 ]]
-then
-  echo "Conditions OK!"
+if diff -q "$file1" "$file2" &>/dev/null; then
+  echo "✅ As condições das policies são IGUAIS!"
 else
-  echo "Conditions not OK!"
-  diff ${policyID[0]}.txt ${policyID[1]}.txt
+  echo "❌ As condições das policies são DIFERENTES!"
+  diff "$file1" "$file2"
 fi
-
-
-
-
